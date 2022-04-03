bde06b4b6b7cd8deb0f2f36d9f7444fa3e9cb471
Pierre COURANT <pcourant.dev@gmail.com>
2022-04-03 05:45:07 +0200

    Add filters buttons structure

--- scripts/factories/RecipeFactory.js
+++ scripts/factories/RecipeFactory.js
@@ -43,6 +43,7 @@ function recipeFactory(data) {
     const link = document.createElement("a");
     article.appendChild(link);
     link.setAttribute("href", "#");
+    link.classList.add("stretched-link");
     const imageCardTop = document.createElement("img");
     link.appendChild(imageCardTop);
     imageCardTop.setAttribute("src", "./assets/images/recipe.jpg");
